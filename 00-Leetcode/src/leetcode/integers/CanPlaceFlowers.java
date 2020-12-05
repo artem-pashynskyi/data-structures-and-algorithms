b5859e343f213de0215285d3d2d6d40afc1b2b93
artem-pashynskyi <mailofartem@gmail.com>
2020-12-05 18:06:17 -0500

    Can Place Flowers

---
+++ 00-Leetcode/src/leetcode/integers/CanPlaceFlowers.java
@@ -0,0 +1,20 @@
+package leetcode.integers;
+
+public class CanPlaceFlowers {
+    public static void main(String[] args) {
+        System.out.println(canPlaceFlowers(new int[]{1,0,0,0,1}, 1));
+    }
+
+    public static boolean canPlaceFlowers(int[] flowerbed, int n) {
+        int emptySlots = 0;
+        for(int i = 0 ; i < flowerbed.length ; i++) {
+            if(flowerbed[i] == 0
+                    && (i == 0 || flowerbed[i-1] == 0)
+                    && (i == flowerbed.length-1 || flowerbed[i+1] == 0)) {
+                emptySlots++;
+                i++;
+            }
+        }
+        return emptySlots >= n;
+    }
+}
